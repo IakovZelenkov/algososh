2d5dcd73f74eb35608303caa41f1c1a6734e277b
Iakov Zelenkov <iakovzelenkov@gmail.com>
2023-09-12 17:47:03 +0300

    initial list component layout

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,89 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import s from "./list-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 
 export const ListPage: React.FC = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [inputIndexValue, setInputIndexValue] = useState("");
+  const [isChanging, setIsChanging] = useState(false);
+
+  const onChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    const newInputValue = evt.target.value;
+    setInputValue(newInputValue);
+  };
+
   return (
     <SolutionLayout title="Связный список">
-
+      <div className={s.container}>
+        <div className={s.menu}>
+          <div className={s.wrapper}>
+            <Input
+              placeholder="Введите значение"
+              extraClass={s.input}
+              onChange={onChange}
+              isLimitText={true}
+              maxLength={4}
+              value={inputValue}
+            />
+            <Button
+              extraClass={s.flexGrow}
+              text="Добавить в head"
+              onClick={() => {}}
+              isLoader={false}
+              disabled={false}
+            />
+            <Button
+              extraClass={s.flexGrow}
+              text="Добавить в tail"
+              onClick={() => {}}
+              isLoader={false}
+              disabled={false}
+            />
+            <Button
+              extraClass={s.flexGrow}
+              text="Удалить из head"
+              onClick={() => {}}
+              isLoader={false}
+              disabled={false}
+            />
+            <Button
+              extraClass={s.flexGrow}
+              text="Удалить из tail"
+              onClick={() => {}}
+              isLoader={false}
+              disabled={false}
+            />
+          </div>
+          <div className={s.wrapper}>
+            <Input
+              extraClass={s.input}
+              placeholder="Введите индекс"
+              onChange={onChange}
+              value={inputValue}
+            />
+            <Button
+              extraClass={s.flexGrow}
+              text="Добавить по индексу"
+              onClick={() => {}}
+              isLoader={false}
+              disabled={false}
+            />
+            <Button
+              extraClass={s.flexGrow}
+              text="Удалить по индексу"
+              onClick={() => {}}
+              isLoader={false}
+              disabled={false}
+            />
+          </div>
+        </div>
+      </div>
+      <div className={s.items}></div>
     </SolutionLayout>
   );
 };
---
+++ src/components/list-page/listUtils.ts
@@ -0,0 +1,84 @@
+export class Node<T> {
+  value: T;
+  next: Node<T> | null;
+  constructor(value: T, next?: Node<T> | null) {
+    this.value = value;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+interface ILinkedList<T> {
+  append: (element: T) => void;
+  insertAt: (element: T, position: number) => void;
+  getSize: () => number;
+  print: () => void;
+}
+
+export class LinkedList<T> implements ILinkedList<T> {
+  private head: Node<T> | null;
+  private size: number;
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  insertAt(element: T, index: number) {
+    if (index < 0 || index > this.size) {
+      console.log("Enter a valid index");
+      return;
+    } else {
+      const newNode = new Node(element);
+
+      if (index === 0) {
+        newNode.next = this.head;
+        this.head = newNode;
+      } else {
+        let current = this.head;
+        let previous = null;
+        let currentIndex = 0;
+
+        while (currentIndex < index) {
+          previous = current;
+          current = current!.next;
+          currentIndex++;
+        }
+
+        newNode.next = current;
+        previous!.next = newNode;
+      }
+
+      this.size++;
+    }
+  }
+
+  append(element: T) {
+    const node = new Node(element);
+    let current;
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  print() {
+    let curr = this.head;
+    let res = "";
+    while (curr) {
+      res += `${curr.value} `;
+      curr = curr.next;
+    }
+    console.log(res);
+  }
+}
