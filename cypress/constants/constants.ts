a520f239f4843840103ac707590c0f9ae6a19b9e
Iakov Zelenkov <iakovzelenkov@gmail.com>
2023-10-09 23:34:19 +0300

    finished list E2E tests

--- cypress/constants/constants.ts
+++ cypress/constants/constants.ts
@@ -1,13 +1,21 @@
 export const SELECTORS = {
   input: '[data-cy="input"]',
+  indexInput: '[data-cy="index-input"]',
   button: '[data-cy="button"]',
   addButton: '[data-cy="add-button"]',
+  addHeadButton: '[data-cy="add-head-button"]',
+  addTailButton: '[data-cy="add-tail-button"]',
+  deleteHeadButton: '[data-cy="delete-head-button"]',
+  deleteTailButton: '[data-cy="delete-tail-button"]',
+  addByIndexButton: '[data-cy="add-index-button"]',
+  deleteByIndexButton: '[data-cy="delete-index-button"]',
   deleteButton: '[data-cy="delete-button"]',
   clearButton: '[data-cy="clear-button"]',
   circleContent: "[class*=circle_content]",
   circle: "[class*=circle_circle]",
   circleIndex: "[class*=circle_index]",
   circleLetter: "[class*=circle_letter]",
+  smallCircle: "[class*=circle_small]",
 };
 
 export const STYLES = {
