0ad4592a1dabac8611ccd66a8bd782ddebb626b0
Iakov Zelenkov <iakovzelenkov@gmail.com>
2023-10-10 01:01:36 +0300

    removed expetedUrl since it is equal to path

--- cypress/e2e/routing.cy.ts
+++ cypress/e2e/routing.cy.ts
@@ -1,43 +1,36 @@
 const ROUTES = [
   {
     path: "/",
-    expectedUrl: "/",
     selector: "h1",
     content: "МБОУ АЛГОСОШ",
   },
   {
     path: "/recursion",
-    expectedUrl: "/recursion",
     selector: "h3",
     content: "Строка",
   },
   {
     path: "/fibonacci",
-    expectedUrl: "/fibonacci",
     selector: "h3",
     content: "Последовательность Фибоначчи",
   },
   {
     path: "/sorting",
-    expectedUrl: "/sorting",
     selector: "h3",
     content: "Сортировка массива",
   },
   {
     path: "/stack",
-    expectedUrl: "/stack",
     selector: "h3",
     content: "Стек",
   },
   {
     path: "/queue",
-    expectedUrl: "/queue",
     selector: "h3",
     content: "Очередь",
   },
   {
     path: "/list",
-    expectedUrl: "/list",
     selector: "h3",
     content: "Связный список",
   },
@@ -52,7 +45,7 @@ describe("Навигация приложения", () => {
     it(`должна открыться страница ${route.content}`, () => {
       cy.visit(route.path);
 
-      cy.url().should("eq", Cypress.config("baseUrl") + route.expectedUrl);
+      cy.url().should("eq", Cypress.config("baseUrl") + route.path);
 
       cy.get(route.selector).should("contain", route.content);
     });
